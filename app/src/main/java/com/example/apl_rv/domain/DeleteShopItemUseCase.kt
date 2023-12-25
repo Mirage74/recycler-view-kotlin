0d87669fd66d2181f7db9fb04ee29722b7474786
balex <balexvicx@gmail.com>
2023-12-25 21:49:49 +0100

    lesson6 .kt and .xml addded

---
+++ app/src/main/java/com/example/apl_rv/domain/DeleteShopItemUseCase.kt
@@ -0,0 +1,8 @@
+package com.example.apl_rv.domain
+
+class DeleteShopItemUseCase(private val shopListRepository: ShopListRepository) {
+
+    fun deleteShopItem(shopItem: ShopItem) {
+        shopListRepository.deleteShopItem(shopItem)
+    }
+}

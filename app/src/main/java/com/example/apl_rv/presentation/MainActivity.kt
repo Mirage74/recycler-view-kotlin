b068705cf9c3255a696e56a492504898dea058fe
balex <balexvicx@gmail.com>
2023-12-30 21:22:33 +0100

    23 linear

--- app/src/main/java/com/example/apl_rv/presentation/MainActivity.kt
+++ app/src/main/java/com/example/apl_rv/presentation/MainActivity.kt
@@ -2,15 +2,22 @@ package com.example.apl_rv.presentation
 
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
 import com.example.apl_rv.R
 import com.example.apl_rv.databinding.ActivityMainBinding
+import com.example.apl_rv.databinding.ItemShopEnabledBinding
+import com.example.apl_rv.databinding.ItemShopDisabledBinding
+import com.example.apl_rv.domain.ShopItem
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var viewModel: MainViewModel
+    private lateinit var llShopList: LinearLayout
     private val TAG = "MainActivity"
     //var shopListAct = MutableList<ShopItem>
     //var shopListAct: List<ShopItem> =Collections.emptyList()
@@ -18,19 +25,35 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        viewModel = ViewModelProvider(this, MainViewModelFactory(application))[MainViewModel::class.java]
+        viewModel =
+            ViewModelProvider(this, MainViewModelFactory(application))[MainViewModel::class.java]
+        llShopList = binding.llShopList
         viewModel.shopList.observe(this) {
-            Log.d(TAG, "it : $it")
-            //Log.d(TAG, "0$shopListAct")
-            if (it[0].id == 0) {
-                //viewModel.deleteShopItem(it[0])
-                val item = it[0]
-                viewModel.changeEnabledState(item)
-            }
-
+            showList(it)
         }
-
-
     }
+
+    private fun showList(list: List<ShopItem>) {
+        val bindingEnableView = DataBindingUtil.setContentView<ItemShopEnabledBinding>(this, R.layout.item_shop_enabled)
+        val bindingDisableView = DataBindingUtil.setContentView<ItemShopDisabledBinding>(this, R.layout.item_shop_disabled)
+        var tvName: TextView
+        var tvCount: TextView
+        for (shopItem in list) {
+            val layoutID = if (shopItem.enabled) {
+//                tvName = bindingEnableView.tvName
+//                tvCount = bindingEnableView.tvCount
+                R.layout.item_shop_enabled
+            } else {
+//                tvName = bindingDisableView.tvName
+//                tvCount = bindingDisableView.tvCount
+                R.layout.item_shop_disabled
+            }
+            val view = LayoutInflater.from(this).inflate(layoutID, llShopList, false)
+//            tvName.text = shopItem.name
+//            tvCount.text = shopItem.count.toString()
+            llShopList.addView(view)
+        }
+    }
+
 
 }
